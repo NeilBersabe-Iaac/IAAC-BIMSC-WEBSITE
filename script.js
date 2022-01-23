ad185eafa7a5db07df9d0dcf328777678773023d
ArchSaber22 <47876822+TruLuvW9s92@users.noreply.github.com>
2022-01-23 22:16:07 +0800

    Update script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+// SCRIPT STRUCTURE
+//----------------------
+//SET IMPORTS
+//1. Import Three.js
+//2. Import Oribit/Camera Controls
+//----------------------
+//SET CONSTANTS AND VARIABLES
+//3. Create a Scene
+//4. Create a Camera
+//5. Create a Renderer
+//6. Create Camera/ Orbit Controls
+//----------------------
+//CREATE OBJECTS
+//7. Define Geometry
+//8. Define Material
+//9. Create Object
+//10. Add it to the Scene
+//----------------------
+//RENDERING
+//11. Define Animations and Renderings
